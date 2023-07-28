efb75b574c3c94b4fda7d1f39f7d39df6fb4f1bb
Zoe Guidry <oyezoey@gmail.com>
2023-07-28 14:42:06 -0500

    lab complete

--- server/werkzeug_app.py
+++ server/werkzeug_app.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+from werkzeug.wrappers import Request, Response
+
+@Request.application
+def application(request):
+    print(f'This web server is running at {request.remote_addr}')
+    return Response('A WSGI generated this response!')
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple('localhost', 5555, application)
